a42f13e70d62031f0290a7c97d7120dcfb927030
vittorio01 <vittoriopolci@live.com>
2025-08-24 23:41:27 +0200

    added lcd library

--- software/software.ino
+++ software/software.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ShiftLcd.h>
+//20x2 lcd, max 38 per line
+int LCD_EN = 4;
+int LCD_D7 = 5; 
+int SHIFT_SER = 6; 
+int SHIFT_SRCLK = 7;
 
+ShiftLcd lcd(LCD_EN, LCD_D7, SHIFT_SER, SHIFT_SRCLK);
+
+void setup() {
+  lcd.begin();
+  lcd.write("Hello, World←000000000000000000000");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.scrollLeft(100);
 }
